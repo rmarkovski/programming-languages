cfcee84fbdfa2ad5b2280683d6c88c73393e37a0
Rastko_Markovski_5492 <146971813+rmarkovski@users.noreply.github.com>
2023-11-22 18:49:10 +0100

    Update Main.java

--- Lecture10/Main.java
+++ Lecture10/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Create an instance of Test
         Test testObject = new Test();
+        
+        System.out.println("Class Name: " + testObject.getClass().getName());
 
-        // Invoke method1 to print the value of s
-        testObject.method1();
+        System.out.println("Constructor Name: " + testObject.getClass().getName());
 
-        // Invoke method2 to print the value of a specific integer (e.g., 42)
-        testObject.method2(42);
-
-        // Private method3 cannot be directly invoked outside the Test class
-        // It can only be called from within other methods of the Test class
+        System.out.println("Method Names:");
+        java.lang.reflect.Method[] methods = Test.class.getDeclaredMethods();
+        for (java.lang.reflect.Method method : methods) {
+            System.out.println(method.getName());
+        }
     }
 }
